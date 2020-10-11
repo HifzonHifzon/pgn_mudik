0383d8085de3756d7af0d419a4049567c3d6cd9e
HifzonHifzon <Hifzon746@gmail.com>
2020-10-11 14:38:45 +0700

    last update

--- application/views/front_end/main.php
+++ application/views/front_end/main.php
@@ -190,13 +190,13 @@
 
       <form class="form-horizontal" action="<?php echo base_url().'main/do_upload_images'?>"  method="POST" enctype='multipart/form-data'>
             <div class="row">
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-4 col-sm-4 col-md-3">
                     Nama Lengkap 
                 </div>
-                <div class="col-xs-3 col-md-1">
+                <div class="col-xs-3 col-sm-1 col-md-1">
                     :
                 </div>
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-3 col-sm-4 col-md-3">
                     <input type="hidden" class="form-control id_berangkat" name="id_berangkat" value="">
                     <input type="text" class="form-control" name="nama_lengkap">
                 </div>
@@ -204,51 +204,51 @@
 
 
             <div class="row">
-                <div class="col-xs-3 col-md-3">
+            <div class="col-xs-4 col-sm-4 col-md-3">
                     Jenis Kelamin
                 </div>
-                <div class="col-xs-3 col-md-1">
+                <div class="col-xs-3 col-sm-1 col-md-1">
                     :
                 </div>
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-5 col-sm-5 col-md-5">
                     <input type="radio"  name="jeniskelamin" value="pria" checked> Pria
                     <input type="radio"  name="jeniskelamin" value="perempuan"> Perempuan
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-xs-3 col-md-3">
+            <div class="col-xs-4 col-sm-4 col-md-3">
                     Email
                 </div>
-                <div class="col-xs-3 col-md-1">
+                <div class="col-xs-3 col-sm-1 col-md-1">
                     :
                 </div>
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-5 col-sm-5 col-md-5">
                     <input type="text" class="form-control" name="email">
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-xs-3 col-md-3">
+            <div class="col-xs-4 col-sm-4 col-md-3">
                    Upload KTP & Foto 
                 </div>
-                <div class="col-xs-3 col-md-1">
+                <div class="col-xs-3 col-sm-1 col-md-1">
                     :
                 </div>
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-5 col-sm-5 col-md-5">
                     <input type="file" name="files[]" multiple="">
                 </div>
             </div>
 
 
             <div class="row">
-                <div class="col-xs-3 col-md-3">
+            <div class="col-xs-4 col-sm-4 col-md-3">
                     Jumlah yang didaftarkan
                 </div>
-                <div class="col-xs-3 col-md-1">
+                <div class="col-xs-3 col-sm-1 col-md-1">
                     :
                 </div>
-                <div class="col-xs-3 col-md-3">
+                <div class="col-xs-5 col-sm-5 col-md-5">
                     <input type="text" name="jumlah_peserta" class="form-control">
                 </div>
             </div>
